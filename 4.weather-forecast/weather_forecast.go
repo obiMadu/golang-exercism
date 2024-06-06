940910584a3bb6c550e9bb85f45fdd92c569512d
Obi.M <mail@obimadu.pro>
2024-06-06 23:49:25 +0100

    docs: fix variable comments positioning

--- 4.weather-forecast/weather_forecast.go
+++ 4.weather-forecast/weather_forecast.go
@@ -1,10 +1,10 @@
 // Package weather forecasts weather conditions for the country of Goblinocus
 package weather
+
 // CurrentCondition variable stores the current weather condition
+var CurrentCondition string
 
 // CurrentLocation variable stores the current location
-
-var CurrentCondition string
 var CurrentLocation string
 
 // Forecast takes in a city and condition parameter and returns a forecast
