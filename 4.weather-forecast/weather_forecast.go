cf232b76fcc33bf08c62c1efd032ffa5b960615b
Obi.M <mail@obimadu.pro>
2024-06-06 23:50:59 +0100

    style: fix documentation comments syntax

--- 4.weather-forecast/weather_forecast.go
+++ 4.weather-forecast/weather_forecast.go
@@ -1,13 +1,13 @@
-// Package weather forecasts weather conditions for the country of Goblinocus
+// Package weather forecasts weather conditions for the country of Goblinocus.
 package weather
 
-// CurrentCondition variable stores the current weather condition
+// CurrentCondition variable stores the current weather condition.
 var CurrentCondition string
 
-// CurrentLocation variable stores the current location
+// CurrentLocation variable stores the current location.
 var CurrentLocation string
 
-// Forecast takes in a city and condition parameter and returns a forecast
+// Forecast takes in a city and condition parameter and returns a forecast.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
