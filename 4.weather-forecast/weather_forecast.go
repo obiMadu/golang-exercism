7ddcc5a678a9ce054e25de6c8f8074fb39011233
Obi.M <mail@obimadu.pro>
2024-06-06 23:46:58 +0100

    docs: comment the forecast function

--- 4.weather-forecast/weather_forecast.go
+++ 4.weather-forecast/weather_forecast.go
@@ -7,6 +7,7 @@ package weather
 var CurrentCondition string
 var CurrentLocation string
 
+// Forecast takes in a city and condition parameter and returns a forecast
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
