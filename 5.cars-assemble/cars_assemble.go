076e144778c97ad61803072186580965191deab5
Obi.M <mail@obimadu.pro>
2024-06-07 09:21:43 +0100

    docs: fix documentation comment syntax

--- 5.cars-assemble/cars_assemble.go
+++ 5.cars-assemble/cars_assemble.go
@@ -14,10 +14,10 @@ func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int 
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	// CostofEachTenCars contains the total amount for each group of ten cars
+	// CostofEachTenCars contains the total amount for each group of ten cars.
 	costOfEachTenCars := (carsCount / 10) * 95000
 
-	// CostofTheRemainingCars contains the total amount for other cars after the last group of 10
+	// CostofTheRemainingCars contains the total amount for other cars after the last group of 10.
 	costofTheRemainingCars := (carsCount % 10) * 10000
 
 	return uint(costOfEachTenCars + costofTheRemainingCars)
