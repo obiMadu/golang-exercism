3b2ec4cb5f1d4e66c4e36779468fde4d0cd9e890
Obi.M <mail@obimadu.pro>
2024-06-08 12:10:45 +0100

    feat: implement choose vehicle func

--- 8.vehicle-purchase/vehicle_purchase.go
+++ 8.vehicle-purchase/vehicle_purchase.go
@@ -1,5 +1,7 @@
 package purchase
 
+import "fmt"
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
 	if kind == "car" || kind == "truck" {
@@ -10,7 +12,10 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	if option1 < option2 {
+		return fmt.Sprintf("%s is clearly the better choice.", option1)
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", option2)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
