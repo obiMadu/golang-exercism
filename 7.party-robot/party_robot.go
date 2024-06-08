2c0f38631dd3fae921dea1a55db344449d6577f7
Obi.M <mail@obimadu.pro>
2024-06-08 11:29:13 +0100

    refactor: make assigntable func more readable

--- 7.party-robot/party_robot.go
+++ 7.party-robot/party_robot.go
@@ -14,5 +14,8 @@ func HappyBirthday(name string, age int) string {
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	return fmt.Sprintf("Welcome to my party, %s!\nYou have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\nYou will be sitting next to %s.", name, table, direction, distance, neighbor)
+	assign := fmt.Sprintf("Welcome to my party, %s!\n", name)
+	assign += fmt.Sprintf("You have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\n", table, direction, distance)
+	assign += fmt.Sprintf("You will be sitting next to %s.", neighbor)
+	return assign
 }
